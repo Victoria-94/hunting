2d0c7de49d7fdf438be1d9070bdb77d0eaef76d9
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-05-12 21:39:26 -0500

    completo crud

---
+++ src/creatures/dto/create-creature.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateCreatureDto {
+    @ApiProperty()
+    name:string;
+
+    @ApiProperty({required:false})
+    description?:string;
+    @ApiProperty()
+    lastSee:string;
+
+    @ApiProperty()
+    countLastSee:number;
+    @ApiProperty({required:false, default:false})
+    extinct?:boolean=false;
+}
